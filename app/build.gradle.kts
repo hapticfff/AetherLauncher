32d8d07cb4024c0dde199080de2010e588105b0f
hapticfff <theprofessionalgaming4@gmail.com>
2026-09-08 09:48:20 +0530

    Fix Java runtime execution on Android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.example.aetherlauncher"
         minSdk = 23
-        targetSdk = 35
+        targetSdk = 28
         versionCode = 2
         versionName = "0.2.0"
     }
